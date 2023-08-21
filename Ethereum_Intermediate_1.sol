04e3a722ec427229e1414bbf96c0d96910e62b1d
Aryan7075 <112176831+Aryan7075@users.noreply.github.com>
2023-08-21 23:50:32 +0530

    Update Smart Contract

--- Ethereum_Intermediate_1.sol
+++ Ethereum_Intermediate_1.sol
@@ -1,36 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.0;
 
-// write a smart contract that implements the require(), assert() and revert() statements.
-
-contract errorHandling
+contract ErrorHandling 
 {
+    address owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
-    // address variable that stores the address of the owner
-    address owner = 0x56C1c21AAD780253f0648Deee1C7113eE2cA4215;
-
-    function testAssert(address _z) public view returns (address)
+    function testAssert(address _z) public view returns (address) 
     {
-        // checks if the person deploying the contract is the owner
-        assert(msg.sender==_z);
+        // Using assert to check if the sender is the owner
+        assert(msg.sender == owner);
         return _z;
+    }
 
     function testRevert(uint _x) public pure returns (uint) 
     {
-            // checks if the value of _x is greater than 10 or not and reverts an error if it is not
-            if(_x<10)
-            {
-                revert("enter a value greater than 10");
-            }
-            return _x;
+        // Checking if _x is greater than 10, otherwise revert
+        if (_x <= 10) {
+            revert("Value must be greater than 10");
+        }
+        return _x;
     }
 
     function testRequire(uint numerator, uint denominator) public pure returns (uint) 
-    {    
-        // checks if the denominator is not equal to 0, and returns an error if it is zero or returns the quotient if it is not equal to 0
+    {
+        // Using require to check if denominator is not zero
         require(denominator != 0, "Denominator cannot be zero");
         uint _z = numerator / denominator;
         return _z;
     }
-
 }
